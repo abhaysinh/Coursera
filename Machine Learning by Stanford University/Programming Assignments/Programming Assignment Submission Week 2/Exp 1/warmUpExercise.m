0da59a044bfad9bff2a852200440ba7a2b5e3494
Abhaysinh <abhaysinh.surve@gmail.com>
2020-07-30 13:41:24 +0530

    First Commit

---
+++ Machine Learning by Stanford University/Programming Assignments/Programming Assignment Submission Week 2/Exp 1/warmUpExercise.m
@@ -0,0 +1,24 @@
+function A = warmUpExercise()
+%WARMUPEXERCISE Example function in octave
+% function [y1,...,yN] = myfun(x1,...,xN)
+% above function 'myfun' takes argument (x1,...,xN) and returns y1,...,yN
+% Return the 5x5 identity matrix in octave
+%   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+
+A = eye(5);
+% ============= YOUR CODE HERE ==============
+% Instructions: Return the 5x5 identity matrix 
+%               In octave, we return values by defining which variables
+%               represent the return values (at the top of the file)
+%               and then set them accordingly. 
+
+
+
+
+
+
+
+% ===========================================
+
+
+end
